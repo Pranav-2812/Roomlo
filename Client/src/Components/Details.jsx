4eddbc3940d36b4f6b6ac5c86a5eb00c7af471d1
Pranav <pranavdhumale2812@gmail.com>
2024-09-13 19:33:36 +0530

    12/9/24 details section

--- Client/src/Components/Details.jsx
+++ Client/src/Components/Details.jsx
@@ -9,7 +9,7 @@ const Details = () => {
         <div className="col">
           <div className="row mt-5" id="details-img-card">
             <div className="w-75">
-              <img src={Room} className="h-75 w-100" alt="..." />
+              <img src={Room} className="h-75 w-100 rounded-3" alt="..." />
             </div>
             <div className="card w-25" >
               <div className="card-body">
@@ -89,12 +89,12 @@ const Details = () => {
               <h3>Double Sharing</h3>
               <h4 className="ms-2" style={{ color: "grey", marginBottom: "5px" }}>(Bachlor)</h4>
             </span>
-            <span className="d-flex flex-row align-items-center" style={{color:"grey"}}>
+            <span className="d-flex flex-row align-items-center" style={{ color: "grey" }}>
               <i className="fa-solid fa-location-dot me-2 pb-2"></i>
               <h4>900m</h4>
             </span>
             <p>Available from :21 March 2024</p>
-            <p><a href="#" class="link-underline-secondary">Name: Giradkar</a></p>
+            <p><a href="#" className="link-underline-secondary">Name: Giradkar</a></p>
           </div>
           <div className="col w-25">
             <div className="d-flex justify-content-end">
@@ -109,6 +109,93 @@ const Details = () => {
 
         </div>
       </div>
+      <div className="col container">
+        <div className="link-container">
+          <a className="details-links" href="#about">About</a>
+          <a className="details-links" href="#address">Address</a>
+          <a className="details-links">Contact</a>
+          <a className="details-links">Similar Propeties</a>
+        </div>
+        <div className="my-4" id="about">
+          <h2>About</h2>
+            <div className="w-75 bg-info bg-opacity-25 rounded-5 my-4">
+              <div className="d-flex flex-row w-100 px-4 py-2">
+                <div className="d-flex flex-row mx-2 my-2 w-50 align-items-center  even">
+                  <h4 className="w-50">Area:</h4>
+                  &ensp;&ensp;
+                  <p className="w-50">250sq.ft    (Medium) </p>
+                </div>
+                <div className="d-flex flex-row mx-2 my-2 w-50 align-items-center  odd">
+                <h4 className="w-50">Electric Bill:</h4>
+                &ensp;&ensp;
+                <p className="w-50">Include </p>
+                </div>
+              </div>
+              <div className="d-flex flex-row w-100 px-4 py-2">
+                <div className="d-flex flex-row mx-2 my-2 w-50  align-items-center even">
+                  <h4 className="w-50">Wall:</h4>
+                  &ensp;&ensp;
+                  <p className="w-50">Putting  (White) </p></div>
+                <div className="d-flex flex-row mx-2 my-2 w-50 align-items-center  odd">
+                <h4 className="w-50">Non Veg:</h4>
+                &ensp;&ensp;
+                <p className="w-50">Yes allow </p>
+                </div>
+              </div>
+              <div className="d-flex flex-row w-100 px-4 py-2">
+                <div className="d-flex flex-row mx-2 my-2 w-50 align-items-center  even">
+                  <h4 className="w-50">Floor:</h4>
+                  &ensp;&ensp;
+                  <p className="w-50">1-3 Floor </p>
+                </div>
+                <div className="d-flex flex-row mx-2 my-2 w-50  align-items-center odd">
+                <h4 className="w-50">Wifi:</h4>
+                &ensp;&ensp;
+                <p className="w-50">Yes </p>
+                </div>
+              </div>
+              <div className="d-flex flex-row w-100 px-4 py-2">
+                <div className="d-flex flex-row mx-2 my-2 w-50  align-items-center even">
+                  <h4 className="w-50">Tiles:</h4>
+                  &ensp;&ensp;
+                  <p className="w-50">Yes </p>
+                </div>
+                <div className="d-flex flex-row mx-2 my-2 w-50 align-items-center  odd">
+                <h4 className="w-50">Aminities:</h4>
+                &ensp;&ensp;
+                <p className="w-50">Bed,Fan,Almari, Light</p>
+                </div>
+              </div>
+              <div className="d-flex flex-row w-100 px-4 py-2">
+                <div className="d-flex flex-row mx-2 my-2 w-50 align-items-center even ">
+                  <h4 className="w-50">Parking:</h4>
+                  &ensp;&ensp;
+                  <p className="w-50">2 wheller,4 wheeler </p>
+                </div>
+                <div className="d-flex flex-row mx-2 my-2 w-50 align-items-center  odd"></div>
+              </div>
+              <div className="d-flex flex-row w-100 px-4 py-2">
+                <div className="d-flex flex-row mx-2 my-2 w-50 align-items-center  even">
+                  <h4 className="w-50">Bathroom:</h4>
+                  &ensp;&ensp;
+                  <p className="w-50">2</p>
+                </div>
+                <div className="d-flex flex-row mx-2 my-2 w-50  align-items-center odd"></div>
+              </div>
+              <div className="d-flex flex-row w-100 px-4 py-2">
+                <div className="d-flex flex-row mx-2 my-2 w-50  align-items-center even">
+                  <h4 className="w-50">Toilet:</h4>
+                  &ensp;&ensp;
+                  <p className="w-50">2 </p>
+                </div>
+                <div className="d-flex flex-row mx-2 my-2 w-50  align-items-center odd"></div>
+              </div>
+            </div>
+            <h3 className="link link-danger" style={{cursor:"pointer"}}>View more details -&gt;</h3>
+        </div>
+        <hr className="active" style={{border:"10px solid grey"}}></hr>
+        <div className="my-4" id="address"></div>
+      </div> 
     </div>
   );
 };
