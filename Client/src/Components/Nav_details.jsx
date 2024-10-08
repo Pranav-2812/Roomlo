31140ce8c277011f7389e8db672c9b7d808116c3
Pranav <pranavdhumale2812@gmail.com>
2024-10-08 19:17:58 +0530

    nav-details

--- Client/src/Components/Nav_details.jsx
+++ Client/src/Components/Nav_details.jsx
@@ -4,17 +4,19 @@ import "../Styles/NavDetails.css"; // Import the CSS file
 const Nav_details = () => {
   return (
     <>
-      <div className="container p-3">
+      <div className="container  p-3">
         <h2 className="text-center my-4 py-2">Details</h2>
         <div className="card w-full sm:w-4/5 md:w-3/4 lg:w-2/3 mx-auto card-custom">
-          <div className="card-body rounded p-4" style={{ backgroundColor: "#fffdd0" }}>
+          <div className="card-body rounded  p-4" style={{ backgroundColor: "#fffdd0" }}>
             <h4 className="card-title text-center fw-bold text-base sm:text-lg md:text-xl">1000+</h4>
-            <h4 className="card-subtitle text-center fw-bold text-base sm:text-lg md:text-xl">Tenants/Want Room</h4>
+            <h4 className="card-subtitle text-center pb-2 fw-bold text-base sm:text-lg md:text-xl">Tenants/Want Room</h4>
 
-            <ul className="m-3 text-sm sm:text-base md:text-lg font-semibold">
-              <li className="mb-2">
-                <h5>20%</h5>
-                <span>Rooms</span>
+            <ul className="text-sm sm:text-base md:text-lg font-semibold">
+              <li>
+                <span className="d-flex flex-row align-items-baseline w-100">
+                  <p className="fs-6 me-2">20%</p>
+                  <span>Rooms</span>
+                </span>
               </li>
               <li>Roomlo will take 20% of 1 month rent only.</li>
               <li>Deposit money for Roomlo will be only one month rent.</li>
@@ -28,7 +30,7 @@ const Nav_details = () => {
             </ul>
           </div>
         </div>
-        </div>
+      </div>
     </>
   );
 };
