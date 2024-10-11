e7463cbf9c9d8a1a7b0bdc0da95522b9426fb2ae
devanshukothe <devanshukothe123@gmail.com>
2024-10-11 22:24:54 +0530

    Home page

--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
     <>
       <div className="home">
         <div className="bg-image">
-          <nav className="navbar navbar-light bg-transparent py-3">
-            <div className="container-fluid p-4 m-4">
-              <h2 className="navbar-brand px-4 fs-1">
+          <nav className="navbar navbar-light  bg-transparent py-1">
+            <div className="container-fluid p-2 m-4  ">
+              <h2 className="d-flex align-items-center navbar-brand px-4 fs-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="50"
@@ -22,19 +22,20 @@ const Home = () => {
                 >
                   <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
                 </svg>
-
+                 <span className="roomlo">
                 <img
+                
                   src={logo}
                   alt="logo"
                   width="50"
                   height="50"
                   className="d-inline-block align-text-top pl-3"
                 />
-                &ensp; RoomLo
+                &ensp;RoomLo</span>
               </h2>
-              <h3 className="btn btn-dark rounded-pill fs-5">
+              <h3 className="btn btn-dark rounded-pill fs-5 d-flex align-items-center justify-content-center">
                 <Link
-                  className="text-decoration-none text-light p-4"
+                  className="text-decoration-none text-light px-2"
                   to={"/login"}
                 >
                   Login
@@ -44,34 +45,35 @@ const Home = () => {
           </nav>
 
           {/* Center section */}
-          <div className="container">
+          <div id="cnt">
+          <div className="container" >
             <h1
-              className="text-center text-light fw-bold"
-              style={{ fontSize: "80px" }}
+              className="text-center text-light fw-bold" id="Title"
+              
             >
               Get Rooms
             </h1>
             <h1
               className="text-center text-light fw-bold"
-              style={{ fontSize: "80px" }}
+              id="Title"
             >
               Anywhere In India.
             </h1>
 
             <div
-              className="container px-1 bg-dark w-25 d-flex flex-column justify-content-center"
-              style={{ borderRadius: "20%" }}
+              className="container d-flex flex-column justify-content-center align-items-center"
+             
             >
               <div
-                className="row gx-5 flex-column align-items-center"
-                style={{ borderRadius: "15%" }}
+                className="row flex-column align-items-center" 
+                id="box"
               >
-                <h3 className="text-white text-center pt-3 pb-1">
+                <h3 className="text-white text-center pt-3 pb-1 fs-3">
                   Who Are You?
                 </h3>
 
                 {/* Owner button */}
-                <div className="col pb-4 px-4 w-75">
+                <div className="col pb-4 px-5 w-100">
                   <Link to={"/login"} className="text-decoration-none">
                     <div className="p-2 py-2 border bg-light text-center rounded-pill">
                       <h4 className="text-center text-dark fw-bold">Owner</h4>
@@ -83,7 +85,7 @@ const Home = () => {
                 </div>
 
                 {/* Tenants button */}
-                <div className="col pb-3 px-4 w-75">
+                <div className="col pb-3 px-5 pb-4 w-100" >
                   <Link to={"/login"} className="text-decoration-none">
                     <div className="p-2 border bg-light text-center rounded-pill">
                       <h4 className="text-center text-dark fw-bold">Tenants</h4>
@@ -98,7 +100,7 @@ const Home = () => {
           </div>
 
           {/* Bottom Right Stats */}
-          <div className="position-absolute bottom-0 end-0 mb-5 me-5 text-white">
+          <div className="bottom-0 end-0 mb-5 me-5 text-white" id="btm">
             <div className="d-flex flex-column text-center">
               <div className="row gx-5">
                 <div className="col">
@@ -113,6 +115,7 @@ const Home = () => {
                 </div>
               </div>
             </div>
+          </div>
           </div>
         </div>
       </div>
